e548a82fed078b492bca4f5966dbfa79083c0735
pratyushagudigar <74775443+pratyushagudigar@users.noreply.github.com>
2021-06-17 20:08:58 +0530

    Add files via upload

---
+++ Launcher.js
@@ -0,0 +1,39 @@
+class Launcher{
+    constructor(bodyA, pointB){
+        var options = {
+            bodyA: bodyA,
+            pointB: pointB,
+            stiffness: 0.004,
+            length: 10
+        }
+        
+        this.pointB = pointB
+        this.sling = Constraint.create(options);
+        World.add(world, this.sling);
+
+    }
+attach(body)
+{
+   this.sling.bodyA = body;
+}
+
+fly()
+	{
+	   this.sling.bodyA= null;
+	}
+
+
+
+    display(){
+       
+
+        if(this.sling.bodyA){
+            var pointA = this.sling.bodyA.position;
+            var pointB = this.pointB;
+            strokeWeight(6);
+            line(pointA.x-20, pointA.y, pointB.x-10, pointB.y);
+            line(pointA.x-20, pointA.y, pointB.x+20, pointB.y);
+        }
+    }
+    
+}
